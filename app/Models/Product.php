4768007ea394f268901b1a32dd76cb45641289a0
emon2021 <mdemonahmed2021@gmail.com>
2024-03-13 23:46:48 +0600

    making relationship with category and subcategory model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -9,31 +9,42 @@ class Product extends Model
 {
     use HasFactory;
     protected $fillable = [
-            'name',
-            'code',
-            'unit',
-            'brand_id',
-            'childcategory_id',
-            'category_id',
-            'subcategory_id',
-            'tags',
-            'purchase_price',
-            'selling_price',
-            'discount_price',
-            'stock_quantity',
-            'warehouse',
-            'description',
-            'thumbnail',
-            'images',
-            'video',
-            'featured',
-            'today_deal',
-            'flash_deal_id',
-            'status',
-            'cash_on_delivery',
-            'admin_id',
-            'color',
-            'size',
-            'pickup_point_id',
-        ];
+        'name',
+        'code',
+        'unit',
+        'brand_id',
+        'childcategory_id',
+        'category_id',
+        'subcategory_id',
+        'tags',
+        'purchase_price',
+        'selling_price',
+        'discount_price',
+        'stock_quantity',
+        'warehouse',
+        'description',
+        'thumbnail',
+        'images',
+        'video',
+        'featured',
+        'today_deal',
+        'flash_deal_id',
+        'status',
+        'cash_on_delivery',
+        'admin_id',
+        'color',
+        'size',
+        'pickup_point_id',
+    ];
+
+    //______relation with category model_______
+    public function category()
+    {
+        return $this->belongsTo(Category::class,'category_id');
+    }
+    //______relation with subcategory model_______
+    public function subcategory()
+    {
+        return $this->belongsTo(SubCategory::class,'subcategory_id');
+    }
 }
