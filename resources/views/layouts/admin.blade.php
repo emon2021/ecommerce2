4f7a83490c00afcd0147145e4addc5a04ea0c6e7
emon2021 <mdemonahmed2021@gmail.com>
2024-03-26 13:37:11 +0600

    admin dashboard favicon implement done

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -9,7 +9,7 @@
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome -->
   <link rel="stylesheet" href="{{asset('public/backend')}}/plugins/fontawesome-free/css/all.min.css">
-  
+  <link rel="shortcut icon" href="{{asset($setting->favicon)}}" type="image/x-icon">
   <!-- Ionicons -->
   <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
   <!-- Tempusdominus Bootstrap 4 -->
