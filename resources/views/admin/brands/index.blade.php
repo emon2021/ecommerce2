f294dec2fc58ad6624913d73e358920f83a6c274
emon2021 <mdemonahmed2021@gmail.com>
2024-10-14 19:12:32 +0600

    add to cart realtime using ajax request done

--- resources/views/admin/brands/index.blade.php
+++ resources/views/admin/brands/index.blade.php
@@ -51,7 +51,6 @@
             <div class="container-fluid">
                 <div class="row">
                     <div class="col-12">
-
                         <div class="card">
                             <div class="card-header"> <h3 class="card-title">All Brand list here</h3>
                             </div>
